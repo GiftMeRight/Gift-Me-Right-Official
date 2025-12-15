c91cc0e6b4add882aef7c395af307edf2113be9c
GiftMeRight <l.curan20@gmail.com>
2025-12-15 11:50:27 -0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,10 @@ import './globals.css'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+  <head>
+    <link rel="icon" href="/favicon.jpg" />
+  </head>
+
       <body className="p-6">
         <header className="mb-6">
           <h1 className="text-2xl font-bold">Gift Me Right</h1>
