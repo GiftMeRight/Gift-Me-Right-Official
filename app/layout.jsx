95ef43ed94f57c86e42ed404aa54433a0c4845a5
GiftMeRight <l.curan20@gmail.com>
2025-12-15 11:55:48 -0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,13 +5,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
   <head>
-    <link rel="icon" href="/favicon.jpg" />
+    <link rel="icon" href="/GMR favicon.jpg" />
   </head>
 
       <body className="p-6">
         <header className="mb-6">
           <img
-  src="/logo.jpg"
+  src="/GMR LOGO with White Dots.jpg"
   alt="Gift Me Right logo"
   className="h-10"
 />
